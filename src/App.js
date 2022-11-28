7a714ceb15e6d48ca72e062feae6a317794a5bd2
U1157546 <jan.pavlat@marsh.com>
2022-11-28 11:23:26 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+    return (
+        <div>
+            <h2>Helloo!!</h2>
+            <p>THis is pain</p>
+            <ExpenseItem/>
+        </div>
+    );
+}
+
+export default App;
